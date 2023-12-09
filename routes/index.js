92190ffd0498d9b4d657c1200e6e743bbfd6ccab
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-12-09 12:10:50 +0530

    Log in form is complete

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,8 @@
-var express = require('express');
-var router = express.Router();
-const userModel = require('./users')
-const postModel = require('./post');
-const passport = require('passport');
-const localStrategy = require('passport-local');
-passport.authenticate(new localStrategy(userModel.authenticate()))
+var express = require('express')
+const { model } = require('mongoose')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/profile',isLoggedIn, function (req, res, next) {
-  res.send("Profile")
-});
-
-
-
-router.get('/register', function (req, res) {
-  const { username, email, fullname } = req.body;
-  const userData = new userModel({ username, email, fullname });
-
-  userModel.register(userData, req.body.password)
-    .then(function () {
-      passport.authenticate("local")(req, res, function () {
-        res.redirect('/profile')
-      })
-    })
+router.get('/',function(req,res){
+  res.render('index')
 })
-
-
-router.get('/login',
-  passport.authenticate("local", {
-    successRedirect: '/profile',
-    failureRedirect: '/',
-  }), function (req, res, next) {
-  });
-
-
-router.get('/logout', function (req, res) {
-  req.logout(function (err) {
-    if (err) { return next(err); }
-    res.redirect('/');
-  });
-});
-
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.redirect('/');
-}
-
-
-
 module.exports = router;
